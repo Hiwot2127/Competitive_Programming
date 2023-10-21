4c0dfffc05cdd6545cb676384a1bf9ced80a0d60
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2023-10-21 10:40:54 +0300

    Update and rename Multiply_Strings to Multiply_Strings.py

---
+++ Multiply_Strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        res=0
+        one=len(num1)
+        two=len(num2)
+
+        for i in range (one):
+            a= 10**i
+            for j in range(two):
+                res+=(ord(num1[one-1-i])- ord('0'))*(ord(num2[two-1-j])-ord('0'))*a 
+                a*=10
+        return str(res)
