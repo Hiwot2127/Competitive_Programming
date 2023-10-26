fd598a0a0cafa8ed14b9e2a1fb31ec9145aa9718
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2023-10-26 07:05:38 +0300

    Time: 102 ms (58.23%) | Memory: 16.2 MB (97.43%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,17 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        prev = {}
+        window = 0
+        
+        for i, char in enumerate(s):
+            prev[char] = prev.get(char, 0) + 1
+
+            if window+1 - max(prev.values()) <= k:
+                window += 1
+            else:
+                prev[s[i-window]] -= 1
+        
+        return window
+        
+  
