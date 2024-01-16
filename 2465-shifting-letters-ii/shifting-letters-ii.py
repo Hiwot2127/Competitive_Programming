fe59685b45a1b783c6ee28b8bdcc5638b103c89a
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2024-01-16 19:11:46 +0300

    Time: 1017 ms (97.18%) | Memory: 42.5 MB (30.52%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -1,26 +1,25 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+            prefix = [0] * (len(s)+1)
 
-        prefix = [0] * (len(s)+1)
+            for start, end, direct in shifts:
+                counter = 1 if direct == 1 else -1
+                prefix[start]+=counter
+                prefix[end+1]-=counter
 
-        for start, end, direct in shifts:
-            counter = 1 if direct == 1 else -1
-            prefix[start]+=counter
-            prefix[end+1]-=counter
+            for i in range(1, len(prefix)):
+                prefix[i]+=prefix[i-1]
 
-        for i in range(1, len(prefix)):
-            prefix[i]+=prefix[i-1]
+            res = ""
 
-        res = ""
+            for i in range(len(s)):
+                new_val = (ord(s[i]) + prefix[i]%26)
 
-        for i in range(len(s)):
-            new_val = (ord(s[i]) + prefix[i]%26)
-
-            if 97 <= new_val <= 122:
-                res+=chr(new_val)
-            elif new_val > 122:
-                res+=chr(new_val%123 + 97)
-            else:
-                res+=chr(new_val+26)
-        return res
-        
+                if 97 <= new_val <= 122:
+                    res+=chr(new_val)
+                elif new_val > 122:
+                    res+=chr(new_val%123 + 97)
+                else:
+                    res+=chr(new_val+26)
+            return res
+            
