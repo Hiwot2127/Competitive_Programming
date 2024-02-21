4b90fc62892379c3864a9b7b91d28e809143d493
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2024-02-21 17:49:53 +0300

    Add solution for Partition Labels

---
+++ Week1/leetcode/partition-labels.py
@@ -0,0 +1,17 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        start = 0
+        end = 1
+        ans = []
+        while end < len(s):
+            if not set(s[start:end]) & set(s[end:]):
+                ans.append(len(s[start:end]))
+                start = end
+                end += 1
+            else:
+                end +=1
+        else: 
+            ans.append(len(s[start:end]))
+            
+        return ans
+                 
