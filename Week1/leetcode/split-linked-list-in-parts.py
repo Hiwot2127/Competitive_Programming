c33eaaf5f3b6810e8b6b29482c21720cbe54ef26
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2024-02-21 16:47:07 +0300

    Add solution for Split Linked List in Parts

--- Week1/leetcode/split-linked-list-in-parts.py
+++ Week1/leetcode/split-linked-list-in-parts.py
@@ -1,31 +1,30 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def splitListToParts(self, head, k):
-        """
-        :type head: ListNode
-        :type k: int
-        :rtype: List[ListNode]
-        """
-        length = 0
-        curr = head
-        while curr:
-            length += 1
-            curr = curr.next
-        num_per_part, remainder = divmod(length, k)
+class Solution:
+    def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
+        l = 0
+        temp = head
+        while temp:
+            l += 1
+            temp = temp.next
+        size = l // k
+        extra = l % k
+        res = []
+        temp = head
         
-        result = []
-        curr = head
         for i in range(k):
-            result.append(curr)
-            part_size = num_per_part + (1 if i < remainder else 0)
-            if part_size > 0:
-                for _ in range(part_size - 1):
-                    curr = curr.next
-                temp = curr.next
-                curr.next = None
-                curr = temp
-        return result
+            if not temp:
+                res.append(None) 
+            else:
+                res.append(temp)
+                c = size + 1 if extra > 0 else size
+                extra -= 1
+                for j in range(c - 1):
+                    temp = temp.next  
+                next_node = temp.next
+                temp.next = None  
+                temp = next_node  
+        return res
