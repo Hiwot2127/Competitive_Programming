b7789a2a19300adeae44ee46977f967ce6bb1279
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2024-02-22 23:25:24 +0300

    Add solution for Score of Parentheses

---
+++ Week1/leetcode/score-of-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack=[]
+        count=0
+        flag=0
+
+        for i in range(len(s)):
+            if s[i]=="(":
+                flag=1
+                stack.append("(")
+            if s[i]==")":
+                if flag==1:
+                    count+= 2**(len(stack)-1)
+                    flag=0
+                stack.pop()
+        return count
+            
+
+        
