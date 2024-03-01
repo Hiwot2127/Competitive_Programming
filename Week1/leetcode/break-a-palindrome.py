6c4b5fe5a0878e1efcbb018125d262c2dfb35d6b
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2024-03-01 23:52:44 +0300

    Add solution for Break a Palindrome

---
+++ Week1/leetcode/break-a-palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        for i in range(len(palindrome)//2):
+                if palindrome[i]!='a':
+                    return palindrome[:i]+'a'+palindrome[i+1:]
+        if len(palindrome)>1:
+             return palindrome[:-1]+'b' 
+        else:
+            return ""
+    
