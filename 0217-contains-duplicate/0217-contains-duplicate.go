79e8766870ea4a0e092f3e0d88350dbaaf13fcff
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2024-07-29 11:10:39 +0300

    Time: 79 ms (62.42%), Space: 10.9 MB (37.17%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.go
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -1,12 +1,10 @@
 func containsDuplicate(nums []int) bool {
     seen := make(map[int]struct{})
-
     for _, num := range nums {
         if _, ok := seen[num]; ok {
             return true 
         }
         seen[num] = struct{}{}
     }
-
     return false
 }
